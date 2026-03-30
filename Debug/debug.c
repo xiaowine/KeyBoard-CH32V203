a2d72f05d5874920c239e88f69f3360e0e0f1b4a
xiao_wine <xiaowine0@gmail.com>
2026-03-31 03:34:09 +0800

    优化编译

--- Debug/debug.c
+++ Debug/debug.c
@@ -25,7 +25,8 @@ static uint16_t p_ms = 0;
  *
  * @return  none
  */
-void Delay_Init (void) {
+void Delay_Init(void)
+{
     p_us = SystemCoreClock / 8000000;
     p_ms = (uint16_t)p_us * 1000;
 }
@@ -39,7 +40,8 @@ void Delay_Init (void) {
  *
  * @return  None
  */
-void Delay_Us (uint32_t n) {
+void Delay_Us(uint32_t n)
+{
     uint32_t i;
 
     SysTick->SR &= ~(1 << 0);
@@ -62,7 +64,8 @@ void Delay_Us (uint32_t n) {
  *
  * @return  None
  */
-void Delay_Ms (uint32_t n) {
+void Delay_Ms(uint32_t n)
+{
     uint32_t i;
 
     SysTick->SR &= ~(1 << 0);
@@ -85,58 +88,61 @@ void Delay_Ms (uint32_t n) {
  *
  * @return  None
  */
-void USART_Printf_Init (uint32_t baudrate) {
+void USART_Printf_Init(uint32_t baudrate)
+{
 #ifdef DEBUG
     GPIO_InitTypeDef GPIO_InitStructure;
-    USART_InitTypeDef USART_InitStructure;
 #endif
 
 #if (DEBUG == DEBUG_UART1)
-    RCC_APB2PeriphClockCmd (RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init (GPIOA, &GPIO_InitStructure);
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 #elif (DEBUG == DEBUG_UART2)
-    RCC_APB1PeriphClockCmd (RCC_APB1Periph_USART2, ENABLE);
-    RCC_APB2PeriphClockCmd (RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init (GPIOA, &GPIO_InitStructure);
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 #elif (DEBUG == DEBUG_UART3)
-    RCC_APB1PeriphClockCmd (RCC_APB1Periph_USART3, ENABLE);
-    RCC_APB2PeriphClockCmd (RCC_APB2Periph_GPIOB, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init (GPIOB, &GPIO_InitStructure);
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
 
 #endif
 
+#ifdef DEBUG
+    USART_InitTypeDef USART_InitStructure;
     USART_InitStructure.USART_BaudRate = baudrate;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
     USART_InitStructure.USART_StopBits = USART_StopBits_1;
     USART_InitStructure.USART_Parity = USART_Parity_No;
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
     USART_InitStructure.USART_Mode = USART_Mode_Tx;
+#endif
 
 #if (DEBUG == DEBUG_UART1)
-    USART_Init (USART1, &USART_InitStructure);
-    USART_Cmd (USART1, ENABLE);
+    USART_Init(USART1, &USART_InitStructure);
+    USART_Cmd(USART1, ENABLE);
 
 #elif (DEBUG == DEBUG_UART2)
-    USART_Init (USART2, &USART_InitStructure);
-    USART_Cmd (USART2, ENABLE);
+    USART_Init(USART2, &USART_InitStructure);
+    USART_Cmd(USART2, ENABLE);
 
 #elif (DEBUG == DEBUG_UART3)
-    USART_Init (USART3, &USART_InitStructure);
-    USART_Cmd (USART3, ENABLE);
+    USART_Init(USART3, &USART_InitStructure);
+    USART_Cmd(USART3, ENABLE);
 
 #endif
 }
@@ -150,10 +156,11 @@ void USART_Printf_Init (uint32_t baudrate) {
  *
  * @return  None
  */
-void SDI_Printf_Enable (void) {
+void SDI_Printf_Enable(void)
+{
     *(DEBUG_DATA0_ADDRESS) = 0;
     Delay_Init();
-    Delay_Ms (1);
+    Delay_Ms(1);
 }
 
 /*********************************************************************
@@ -166,49 +173,57 @@ void SDI_Printf_Enable (void) {
  *
  * @return  size: Data length
  */
-__attribute__ ((used)) int _write (int fd, char *buf, int size) {
+__attribute__ ((used)) int _write(int fd, char* buf, int size)
+{
     int i = 0;
 
 #if (SDI_PRINT == SDI_PR_OPEN)
     int writeSize = size;
 
-    do {
-
+    do
+    {
         /**
          * data0  data1 8 byte
          * data0 The storage length of the lowest byte, with a maximum of 7 bytes.
          */
 
-        while ((*(DEBUG_DATA0_ADDRESS) != 0u)) {
+        while ((*(DEBUG_DATA0_ADDRESS) != 0u))
+        {
         }
 
-        if (writeSize > 7) {
-            *(DEBUG_DATA1_ADDRESS) = (*(buf + i + 3)) | (*(buf + i + 4) << 8) | (*(buf + i + 5) << 16) | (*(buf + i + 6) << 24);
+        if (writeSize > 7)
+        {
+            *(DEBUG_DATA1_ADDRESS) = (*(buf + i + 3)) | (*(buf + i + 4) << 8) | (*(buf + i + 5) << 16) | (*(buf + i + 6)
+                << 24);
             *(DEBUG_DATA0_ADDRESS) = (7u) | (*(buf + i) << 8) | (*(buf + i + 1) << 16) | (*(buf + i + 2) << 24);
 
             i += 7;
             writeSize -= 7;
-        } else {
-            *(DEBUG_DATA1_ADDRESS) = (*(buf + i + 3)) | (*(buf + i + 4) << 8) | (*(buf + i + 5) << 16) | (*(buf + i + 6) << 24);
+        }
+        else
+        {
+            *(DEBUG_DATA1_ADDRESS) = (*(buf + i + 3)) | (*(buf + i + 4) << 8) | (*(buf + i + 5) << 16) | (*(buf + i + 6)
+                << 24);
             *(DEBUG_DATA0_ADDRESS) = (writeSize) | (*(buf + i) << 8) | (*(buf + i + 1) << 16) | (*(buf + i + 2) << 24);
 
             writeSize = 0;
         }
-
-    } while (writeSize);
+    }
+    while (writeSize);
 
 
 #else
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++)
+    {
 #if (DEBUG == DEBUG_UART1)
-        while (USART_GetFlagStatus (USART1, USART_FLAG_TC) == RESET);
-        USART_SendData (USART1, *buf++);
+        while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
+        USART_SendData(USART1, *buf++);
 #elif (DEBUG == DEBUG_UART2)
-        while (USART_GetFlagStatus (USART2, USART_FLAG_TC) == RESET);
-        USART_SendData (USART2, *buf++);
+        while (USART_GetFlagStatus(USART2, USART_FLAG_TC) == RESET);
+        USART_SendData(USART2, *buf++);
 #elif (DEBUG == DEBUG_UART3)
-        while (USART_GetFlagStatus (USART3, USART_FLAG_TC) == RESET);
-        USART_SendData (USART3, *buf++);
+        while (USART_GetFlagStatus(USART3, USART_FLAG_TC) == RESET);
+        USART_SendData(USART3, *buf++);
 #endif
     }
 #endif
@@ -222,10 +237,11 @@ __attribute__ ((used)) int _write (int fd, char *buf, int size) {
  *
  * @return  size: Data length
  */
-__attribute__ ((used)) void *_sbrk (ptrdiff_t incr) {
+__attribute__ ((used)) void* _sbrk(ptrdiff_t incr)
+{
     extern char _end[];
     extern char _heap_end[];
-    static char *curbrk = _end;
+    static char* curbrk = _end;
 
     if ((curbrk + incr < _end) || (curbrk + incr > _heap_end))
         return NULL - 1;
